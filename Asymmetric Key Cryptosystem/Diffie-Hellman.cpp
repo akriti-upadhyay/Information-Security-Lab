0c1b032d89dc388e6a1d97f43f08386c5aa18a8a
akriti-upadhyay <akriti1099@gmail.com>
2022-02-16 09:58:45 +0530

    Diffie hellman complete

--- Asymmetric Key Cryptosystem/Diffie-Hellman.cpp
+++ Asymmetric Key Cryptosystem/Diffie-Hellman.cpp
@@ -1,69 +1,123 @@
 #include<iostream>
 using namespace std;
-#define lli long long int
-lli g ,n ;
 
-// power function implementation
-lli power(lli x, lli y)
+// Array of first 26 prime numbers 
+// A-Z => 26 alphabets (only Uppercase)
+// Pick value of 'g' according to the ASCII value of alphabet character
+int g[26]={2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101};
+
+
+void diffie_helman(string key, int pvtA, int pvtB, int prime)
 {
-    lli result = 1;
-    while (y > 0) {
-        if (y % 2 == 0) // y is even
+    string a, b, sendToB, sendToA;
+    // sendToB = g^x % n
+    // b = sendToB ^ y => (g^x % n)^y => shared key of B
+    // sendToA = g^y % n
+    // a = sendToA ^ x => (g^y % n)^x => shared key of A
+    
+
+    // computing, sendToB = g^x % n
+    for(int i=0; i<key.length(); i++)
+    {
+        long long z = 1;  // z = g^x
+        // keep spaces as it is
+        if(key[i] == ' ')
+            sendToB.push_back(' ');
+        else
         {
-            x = x * x;
-            y = y / 2;
-        }
-        else // y isn't even
-        {
-            result = result * x;
-            y = y - 1;
+            // if ascii(char) is in range A-Z
+            if(key[i]>64 && key[i]<91){
+                // g^x 
+                for(int j=0; j<pvtA; j++)
+                    z = z * g[key[i]-65];
+            }
+            sendToB.push_back(z % prime);
         }
     }
-    return result;
+
+    // computing b = sendToB ^ y
+    for(int i=0; i<sendToB.length(); i++)
+    {
+        long long z = 1;
+        // keep space as it is
+        if(sendToB[i] == ' ')
+            b.push_back(' ');
+        else
+        {
+            // sendTOB ^ y
+            for(int j=0; j<pvtB; j++)
+                z = z * sendToB[i];
+            // ascii to alphabet
+            b.push_back(((z % prime) % 26) + 65);
+        }
+    }
+
+    // computing, sendToA = g^y % n
+    for(int i=0; i<key.length(); i++)
+    {
+        long long z = 1;
+        if(key[i] == ' ')
+            sendToA.push_back(' ');
+        else
+        {
+            if(key[i]>64 && key[i]<91){
+                // g^y
+                for(int j=0; j<pvtB; j++)
+                    z = z * g[key[i]-65];
+            }
+            sendToA.push_back(z % prime);
+        }
+    }
+
+    // computing a = sendToA ^ x
+    for(int i=0; i<sendToA.length(); i++)
+    {
+        long long z = 1;
+        if(sendToA[i] == ' ')
+            a.push_back(' ');
+        else
+        {
+            
+            for(int j=0; j<pvtA; j++)
+                z = z*sendToA[i];
+            // ascii to alphabet
+            a.push_back(((z % prime) % 26) + 65);
+        }
+    }
+    cout<<"\nFinal shared key of A: "<<a<<endl;
+    cout<<"Final shared key of B: "<<b<<endl;
+    if(a == b)
+        cout<<"Shared keys of A and B are equal\n\n";
+    else
+        cout<<"Shared keys of A and B are NOT equal\n\n";
 }
 
-int main(){
-    while(1){
-        // string msg;
-        // cout<<"\nEnter the Message: \n";
-        // cin.ignore();
-        // getline(cin, msg);
-        cout<<"\nEnter the value of G (public): ";
-        cin>>g;
-        cout<<"\nEnter the value of N (public): ";
-        cin>>n;
-        lli x,y;
-        cout<<"\nEnter Alice private key: ";
-        cin>>x;
-        cout<<"\nEnter Bob private key: ";
-        cin>>y;
-        lli aliceSend,bobSend;
-        aliceSend = power(g,x);
-        // send to bob
-        bobSend = power(g,y);
-        // send to alice
-
-        // at Alice end
+int main()
+{
+    // Input the encrypted key
+    string key;
+    // pvtA - private key of A (x)
+    // pvtB - private key of B (y)
+    int pvtA, pvtB;
+    cout<<"\nEnter the key to be exchanged (UPPERCASE ALPHABETS ONLY): \n";
+    getline(cin,key);
+    
+    // n - prime num
+    int n;
+    // Bcoz max. value we can handle is 10^100 (otherwise overflow in datatypes)
+    cout<<"\nEnter a prime number (less than 100)"<<endl;
+    cin>>n; 
 
 
+    cout<<"\nEnter the private keys of A and B, \nNOTE: \n1. priv_key(A) < priv_key(B)\n2. Both the private keys should be <= 10)\n";
+    cout<<"\nEnter private key of A: \n";
+    cin>>pvtA;   // x
+    cout<<"\nEnter private key of B: \n";
+    cin>>pvtB;   // y
 
-
-
-
-
-
-        cout<<"\n\nOriginal Message: "<<msg;
-        cout<<"\nEncrypted Message: "<<encrypt;
-        cout<<"\nDecrypted Message: "<<decrypt;
-        cout<<"\n_________________________________________________________________________________________";
-
-        char choice;
-        cout<<"\nDo you want to continue? (y/n)\n";
-        cin>>choice;
-        if(choice == 'n' || choice == 'N')
-            break;
-    }
+    // Key distribution algorith
+    diffie_helman(key, pvtA, pvtB, n);
+    
     return 0;
 }
 
-// First input should start with " Hello" (an extra space), as cin.ignore() ignore the first character of msg (needed to ignore '\n')
--- Asymmetric Key Cryptosystem/RSA.cpp
+++ Asymmetric Key Cryptosystem/RSA.cpp
@@ -1,195 +1,182 @@
-#include<iostream>
-#include<vector>
-#include<cmath>
+#include <bits/stdc++.h>
 using namespace std;
-#define lli long long int
 
-// Print vector
-void printVector(vector<lli> v){
-    for(int i=0; i<v.size(); i++)
-        cout<<v[i]<<" ";
+int n, z, e, d;
+// Vector to store ASCII values of characters from A-Z 
+vector<int> P;
+
+
+
+// GCD(a,b)
+int gcd(int a, int b)
+{
+    return b == 0 ? a : gcd(b, a % b);
+}
+
+// Print the registers
+void printRegister(vector<int> reg){
+    for(int i=0; i<reg.size(); i++)
+        cout<<reg[i]<<" ";
     cout<<"\n";
 }
 
-// GCD(a,b), where a>b
-int gcd(int a, int b){
-    if(a<b)
-        return gcd(b,a);
-    if(b==0)
-        return a;
-    return gcd(b, a%b);
-}
 
-// Extended Euclidean Algorithm 
-lli gcdExtended(lli a, lli b, lli *x, lli *y) 
-{ 
-    // Base Case 
-    if (a == 0) 
-    { 
-        *x = 0, *y = 1; 
-        return b; 
-    } 
-    // To store results of recursive call 
-    lli x1, y1; 
-    lli gcd = gcdExtended(b%a, a, &x1, &y1); 
-  
-    // Update x and y using results of recursive call 
-    *x = y1 - (b/a) * x1; 
-    *y = x1; 
-  
-    return gcd; 
-}
-
-// Function to find modulo inverse of a 
-lli mulInverse(lli a, lli m) 
-{ 
-    lli x, y; 
-    lli g = gcdExtended(a, m, &x, &y); 
-    if (g != 1) 
-        return -1; 
-    else
-    { 
-        // m is added to handle negative x 
-        int res = (x%m + m) % m; 
-        return res; 
-    } 
-} 
-
-// Power function implementation
-lli power(lli x, lli y)
+// ENCRYPTION
+string encryption(string plain, int p, int q)
 {
-    lli result = 1;
-    while(y>0) {
-        // y is even
-        if(y%2 == 0) 
+    // STEP 2: 
+    n = p*q;
+        
+    // STEP 3: 
+    z = (p-1)*(q-1);
+
+    // STEP 4: 
+    // choose (e) relative prime to z (ie, GCD(e, z) = 1)        
+    for(int i=2;i<z;i++){
+        if(gcd(z,i) == 1)
         {
-            x = x * x;
-            y = y / 2;
-        }
-        // y is odd
-        else 
-        {
-            result = result * x;
-            y = y - 1;
+            e = i;
+            break;
         }
     }
-    return result;
-}
 
-// Converting ASCII values vector to string
-string ASCIItoChar(vector<lli> v){
-    string text = "";
-    for(int i=0; i<v.size(); i++){
-        text += (char)v[i];
-        cout<<v[i]<<" => "<<(char)v[i]<<endl;
+    // STEP 5:
+    // find d (multiplicative inverse of e % z), ie, (e*d)%z = 1     
+    for(int i=2;;i++){
+        if((e*i)%z == 1)
+        {
+            d = i;
+            break;
+        }
     }
-    return text;
-}
 
-// Encryption
-vector<lli> encryption(vector<lli> plainText, lli e, lli n){
-    vector<lli> cipher;
-    for(int i=0; i<plainText.size(); i++){
-        cout<<"pow: "<<power(plainText[i], e)<<endl;
-        cipher.push_back(power(plainText[i], e) % n);
+    // output values
+    cout<<"\ne = "<<e<<",   "<<"n = "<<n<<",   "<<"d = "<<d<<endl;
+
+
+    // Encrypting
+    // Public key: {n, e}
+    // C = P^e % n
+    string result, res, cipher;
+    
+    // converting text form of plain text chars to ASCII equivalent (converting range 65-90 to 0-25)
+    for(int i=0; i<plain.length(); i++)
+    {
+        // if char is b/w A-Z
+        if((int)plain[i]>64 && (int)plain[i]<91)
+            // bringing value in range 0-25
+            res.push_back(plain[i]-65);
+        // keep space as it is
+        if(plain[i]==' ')
+            res.push_back(' ');
+    }
+
+
+    // encryption process => P^e % n
+    for(int i=0; i<res.length(); i++)
+    {
+        long long z = 1; // P^e
+        if(res[i] == ' ')
+        {
+            result.push_back(' ');
+            P.push_back((int)res[i]);
+        }
+        else
+        {
+            // P^e
+            for(int j=0; j<e; j++)
+                z = ((int)res[i]) * z;
+            P.push_back((z % n)/26);
+            result.push_back((z % n)%26);
+        }
+    }
+    // cout<<"P VECTOR: ";
+    // printRegister(P);
+
+
+    // changing ASCII values to alphabetic characters (converting range 0-25 to 65-90)
+    for(int i=0; i<result.length() ;i++)
+    {
+        if((int)result[i]>=0 && (int)result[i]<26)
+            cipher.push_back(result[i]+65);
+        if(result[i]==' ')
+            cipher.push_back(' ');
     }
     return cipher;
 }
 
-// Decryption
-vector<lli> decryption(vector<lli> cipherText, lli d, lli n){
-    vector<lli> decryptedPT;
-    for(int i=0; i<cipherText.size(); i++){
-        cout<<"pow:"<<power(cipherText[i], d)<<endl;
-        decryptedPT.push_back(power(cipherText[i], d) % n);
+
+
+// DECRYPTION
+string decryption(string cipher)
+{
+    // Decryption
+    // Private key: {n, d}
+    // P = C^d % n  
+
+    string result, res, final;
+
+    // converting text form of cipher text chars to ASCII equivalent (converting range 65-90 to 0-25)
+    for(int i=0; i<cipher.length(); i++)
+    {
+        if(cipher[i]>64 && cipher[i]<91)
+            res.push_back((cipher[i]-65) + (P[i]*26));
+        if(cipher[i] == ' ')
+            res.push_back(' ');
     }
-    return decryptedPT;
+
+    // decryption process => C^d % n
+    for(int i=0; i<res.length(); i++)
+    {
+        long long z = 1; // C^d
+        if(res[i] == ' ')
+            result.push_back(' ');
+        else
+        {
+            // C^d
+            for(int j=0; j<d; j++)
+                z = ((int)res[i]) * z;
+            result.push_back(z % n);
+        }
+    }
+
+    // changing ASCII values to alphabetic characters (converting range 0-25 to 65-90)
+    for(int i=0; i<result.length(); i++)
+    {
+        if((int)result[i]>=0 && (int)result[i]<26)
+            final.push_back(result[i] + 65);
+        if(result[i] == ' ')
+            final.push_back(' ');
+    }
+
+    return final;
 }
 
-int main(){
-    while(1){
-        // Taking plain text from user
-        string msg;
-        cout<<"\nEnter the Message: \n";
-        cin.ignore();
-        getline(cin, msg);
-        // store ASCII value of plain text chars in array
-        vector<lli> ptChars = {19};
-        // for(lli i=0; i<msg.size(); i++)
-        //     ptChars.push_back(msg[i]);
-        printVector(ptChars);
 
 
-        // STEP 1: 
-        // take large prime numbers: p, q
-        lli p = 3;
-        lli q = 11;
+int main()
+{
+    // Taking plain text from user
+    string plain;
+    cout<<"\nEnter Plain Text: "<<endl;
+    getline(cin,plain);
+
+
+    // STEP 1: 
+    // take large prime numbers: p, q
+    int p;
+    int q;
+    cout<<"\nEnter two prime numbers p and q (such that, 25 < p*q < 40): \n";
+    // possible values (3,11) (3,13) (5,7)
+    cin>>p>>q;
         
-        // STEP 2: 
-        // n = p * q
-        lli n = p*q;
+    // Encryption
+    string cipher = encryption(plain,p,q);
+    cout<<"\nCipher Text: "<<cipher<<endl;
 
-        // STEP 3: 
-        // z = (p-1)*(q-1);
-        lli z = (p-1)*(q-1);
+    // Decryption
+    string decrypt = decryption(cipher);
+    cout<<"Decrypted Plain Text: "<<decrypt<<"\n\n";
 
-        // STEP 4: 
-        // choose (e) relative prime to z (ie, GCD(e, z) = 1)
-        // collection of relative primes of z
-        vector<lli> relativePrimesOfZ;
-        for(lli i=1; i<z; i++){
-            if(gcd(i,z) == 1)
-                relativePrimesOfZ.push_back(i);
-        }
-        // randomly selecting one coprime (e)
-        lli e = relativePrimesOfZ[rand() % relativePrimesOfZ.size()];
-
-        // STEP 5:
-        // find d (multiplicative inverse of e % z), ie, (e*d)%z = 1
-        lli d = mulInverse(e,z);
-
-        cout<<"\np: "<<p<<"\nq: "<<q<<"\nn: "<<n<<"\nz: "<<z<<"\ne: "<<e<<"\nd: "<<d;
-
-       
-       
-        // ENCRYPTION
-        // Public key: {n, e}
-        // C = P^e % n
-        cout<<"\n___________________________________________________________";
-        cout<<"\nENCRYPTION:\n";
-        vector<lli> encryptASCII = encryption(ptChars, e, n);
-        printVector(encryptASCII);
-        string encrypt = ASCIItoChar(encryptASCII);
-        cout<<"\nCipher text: "<<encrypt;
-        
-       
-       
-        // DECRYPTION
-        // Private key: {n, d}
-        // P = C^d % n
-        cout<<"\n___________________________________________________________";
-        cout<<"\nDECRYPTION:\n";
-        vector<lli> decryptASCII = decryption(encryptASCII, d, n);
-        printVector(decryptASCII);
-        string decrypt = ASCIItoChar(decryptASCII);
-        cout<<"\nDecrypted plain text: "<<decrypt;
-
-
-
-        // OUTPUT
-        cout<<"\n____________________________________________________________";
-        cout<<"\n\nOriginal Message: "<<msg;
-        cout<<"\nEncrypted Message: "<<encrypt;
-        cout<<"\nDecrypted Message: "<<decrypt;
-        cout<<"\n____________________________________________________________";
-
-        char choice;
-        cout<<"\nDo you want to continue? (y/n)\n";
-        cin>>choice;
-        if(choice == 'n' || choice == 'N')
-            break;
-    }
     return 0;
 }
-
-// First input should start with " Hello" (an extra space), as cin.ignore() ignore the first character of msg (needed to ignore '\n')
